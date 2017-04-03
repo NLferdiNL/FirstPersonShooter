bf4856c4049ea126b607fb18a163f4c202e2b2d8
koedijk <17798@ma-web.nl>
2017-04-03 12:50:48 +0200

    Zombie Sync

---
+++ Assets/TurnOffNotNeeded.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Networking;
+
+public class TurnOffNotNeeded : NetworkBehaviour
+{
+    [SerializeField]
+    private MonoBehaviour[]  components;
+    [SerializeField]
+    private MonoBehaviour[] VRcomponents;
+
+    void Awake()
+    {
+        if (NetworkServer.connections.Count > 0)
+        {
+            Debug.Log("Im host");
+        }
+        else
+        {
+            Debug.Log("Im Client of HTC");
+
+
+            components = GetComponentsInChildren<MonoBehaviour>();
+            for (int i = 0; i < components.Length; i++)
+            {
+                if (components[i].ToString().Contains("SteamVR"))
+                {
+                    components[i].enabled = true;
+                }
+            }
+        }
+
+    }
+}
